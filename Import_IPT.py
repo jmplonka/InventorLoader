c86ed7194b8a8d85afb4b2e75ff2f5105bc2a502
jmplonka <jens.m.plonka@gmail.com>
2017-10-19 21:11:27 +0200

    Code Review
    Changed single quoted strings to double quoted string for log messages.

--- Import_IPT.py
+++ Import_IPT.py
@@ -21,7 +21,7 @@ __version__     = '0.4.0'
 __status__      = 'In-Development'
 
 def ReadIgnorable(fname, data):
-	logMessage('\t>>> IGNORED: %r' % ('/'.join(fname)))
+	logMessage("\t>>> IGNORED: %r" % ('/'.join(fname)))
 	logMessage(HexAsciiDump(data), LOG.LOG_DEBUG)
 	return len(data)
 
@@ -52,7 +52,7 @@ def ReadElement(ole, fname, doc, counter, readProperties):
 		if (len(fname) == 1):
 			if (name.startswith('\x05')):
 				if (readProperties):
-					logMessage('%2d: %s' % (counter, path), LOG.LOG_DEBUG)
+					logMessage("%2d: %s" % (counter, path), LOG.LOG_DEBUG)
 					if (name == '\x05Aaalpg0m0wzvuhc41dwauxbwJc'):
 						ReadInventorDocumentSummaryInformation(doc, ole.getproperties(fname, convert_time=True), fname)
 					elif (name == '\x05Zrxrt4arFafyu34gYa3l3ohgHg'):
@@ -61,59 +61,59 @@ def ReadElement(ole, fname, doc, counter, readProperties):
 						ReadOtherProperties(ole.getproperties(fname, convert_time=True), fname)
 				end = len(stream)
 			elif (name == 'UFRxDoc'):
-				logMessage('%2d: %s' % (counter, path), LOG.LOG_DEBUG)
+				logMessage("%2d: %s" % (counter, path), LOG.LOG_DEBUG)
 				end = ReadUFRxDoc(stream)
 			elif (name == 'Protein'):
-				logMessage('%2d: %s' % (counter, path), LOG.LOG_DEBUG)
+				logMessage("%2d: %s" % (counter, path), LOG.LOG_DEBUG)
 				end = ReadProtein(stream)
 			else:
-				logMessage('%2d: %s' % (counter, path), LOG.LOG_DEBUG)
+				logMessage("%2d: %s" % (counter, path), LOG.LOG_DEBUG)
 				end = ReadIgnorable(fname, stream)
 		elif (fname[0]=='CacheGraphics'):
 			end = skip(stream)
 		elif (fname[0]=='RSeStorage'):
 			if (isEmbeddings(fname)):
 				if (name.startswith('\x05')):
-					logMessage('%2d: %s' % (counter, path), LOG.LOG_DEBUG)
+					logMessage("%2d: %s" % (counter, path), LOG.LOG_DEBUG)
 					# ReadOtherProperties(ole.getproperties(fname, convert_time=True), fname)
 					end = skip(stream)
 				elif (name == '\x01Ole'):
-					logMessage('%2d: %s' % (counter, path), LOG.LOG_DEBUG)
+					logMessage("%2d: %s" % (counter, path), LOG.LOG_DEBUG)
 #					end = ReadRSeEmbeddingsOle(stream)
 					end = skip(stream)
 				elif (name == '\x01CompObj'):
-					logMessage('%2d: %s' % (counter, path), LOG.LOG_DEBUG)
+					logMessage("%2d: %s" % (counter, path), LOG.LOG_DEBUG)
 					end = ReadRSeEmbeddingsCompObj(stream)
 				elif (name == 'DatabaseInterfaces'):
-					logMessage('%2d: %s' % (counter, path), LOG.LOG_DEBUG)
+					logMessage("%2d: %s" % (counter, path), LOG.LOG_DEBUG)
 					end = ReadRSeEmbeddingsDatabaseInterfaces(stream)
 				elif (name == 'Contents'):
 #					end = ReadRSeEmbeddingsContents(stream)
 					end = skip(stream)
 				elif (name == 'Workbook'):
-					logMessage('%2d: %s' % (counter, path), LOG.LOG_DEBUG)
+					logMessage("%2d: %s" % (counter, path), LOG.LOG_DEBUG)
 					end = ReadWorkbook(doc, stream, fname[-2], name)
 				else:
-					logMessage('%2d: %s' % (counter, path))
+					logMessage("%2d: %s" % (counter, path))
 					end = ReadIgnorable(fname, stream)
 			else:
 				if (name == 'RSeDb'):
-					logMessage('%2d: %s' % (counter, path), LOG.LOG_DEBUG)
+					logMessage("%2d: %s" % (counter, path), LOG.LOG_DEBUG)
 					end = ReadRSeDb(stream)
 				elif (name == 'RSeSegInfo'):
-					logMessage('%2d: %s' % (counter, path))
+					logMessage("%2d: %s" % (counter, path))
 					if ((model) and (model.RSeDb) and (model.RSeDb.version == 0x1D)):
 						end = ReadRSeSegInfo1D(stream)
 					else:
 						end = ReadRSeSegInfo1F(stream)
 				elif (name == 'RSeDbRevisionInfo'):
-					logMessage('%2d: %s' % (counter, path), LOG.LOG_DEBUG)
+					logMessage("%2d: %s" % (counter, path), LOG.LOG_DEBUG)
 					end = ReadRSeDbRevisionInfo(stream)
 				elif (name.startswith('B')):
 					# Skip! will be handled in 'M'
 					end = skip(stream)
 				elif (name.startswith('M')):
-					logMessage('%2d: %s' % (counter, path), LOG.LOG_DEBUG)
+					logMessage("%2d: %s" % (counter, path), LOG.LOG_DEBUG)
 					fnameB = []
 					for n in (fname):
 						fnameB.append(n)
@@ -122,10 +122,10 @@ def ReadElement(ole, fname, doc, counter, readProperties):
 					dataB = ole.openstream(fnameB).read()
 					ReadRSeMetaDataB(dataB, seg)
 				else:
-					logMessage('%2d: %s' % (counter, path), LOG.LOG_DEBUG)
+					logMessage("%2d: %s" % (counter, path), LOG.LOG_DEBUG)
 					end = ReadIgnorable(fname, stream)
 		else:
-			logMessage('%2d: %s' % (counter, path), LOG.LOG_DEBUG)
+			logMessage("'%2d: %s" % (counter, path), LOG.LOG_DEBUG)
 			end = ReadIgnorable(fname, stream)
 
 	return
@@ -135,7 +135,7 @@ def ListElement(ole, fname, counter):
 
 	path = PrintableName(fname)
 	stream = ole.openstream(fname).read()
-	logMessage('%2d: %s size=%s' % (counter, path, len(stream)), LOG.LOG_ALWAYS)
+	logMessage("%2d: %s size=%s" % (counter, path, len(stream)), LOG.LOG_ALWAYS)
 
 def ReadFile(doc, readProperties):
 	first = 0
@@ -178,10 +178,10 @@ def ReadFile(doc, readProperties):
 			doc.Comment += '\n'
 		doc.Comment = '# %s: read from %s' %(now.strftime('%Y-%m-%d %H:%M:%S'), getInventorFile())
 
-		logMessage('Dumped data to folder: \'%s\'' %(getInventorFile()[0:-4]), LOG.LOG_INFO)
+		logMessage("Dumped data to folder: '%s'" %(getInventorFile()[0:-4]), LOG.LOG_INFO)
 
 		return True
-	logError('Error - %s is not a valid Autodesk Inventor file.' %(infile))
+	logError("Error - '%s' is not a valid Autodesk Inventor file." %(infile))
 	return False
 
 def insertGroup(doc, filename):
@@ -201,7 +201,7 @@ def create3dModel(root, doc):
 	if (FreeCAD.GuiUp):
 		FreeCADGui.getDocument(doc.Name).activeView().viewAxonometric()
 		FreeCADGui.SendMsgToActiveView("ViewFit")
-		logMessage('DONE!', LOG.LOG_ALWAYS)
+		logMessage("DONE!", LOG.LOG_ALWAYS)
 
 	return
 
@@ -212,14 +212,12 @@ def insert(filename, docname, skip = [], only = [], root = None):
 	if (canImport()):
 		try:
 			doc = FreeCAD.getDocument(docname)
-			logMessage('Importing: %s' %(filename), LOG.LOG_ALWAYS)
+			logMessage("Importing: %s" %(filename), LOG.LOG_ALWAYS)
 			setInventorFile(filename)
 
 			if (ReadFile(doc, False)):
 				group = insertGroup(doc, filename)
 				create3dModel(group, doc)
-			else:
-				logError('>>>ERROR - no Autodesk Inventor file: %r!' % getInventorFile())
 		except:
 			open(filename, skip, only, root)
 
@@ -231,19 +229,16 @@ def open(filename, skip = [], only = [], root = None):
 	In addition to insert (import), the iProperties are as well added to the document.
 	'''
 	if (canImport()):
-		logMessage('Reading: %s' %(filename), LOG.LOG_ALWAYS)
+		logMessage("Reading: %s" %(filename), LOG.LOG_ALWAYS)
 		setInventorFile(filename)
 		docname = os.path.splitext(os.path.basename(filename))[0]
 		docname = decode(docname, utf=True)
 		doc = FreeCAD.newDocument(docname)
 		doc.Label = docname
 
-
 		if (ReadFile(doc, True)):
 			group = None # Don't create 3D-Model in sub-group
 			create3dModel(group, doc)
-		else:
-			logError('>>>ERROR - no Autodesk Inventor file: %r!' % getInventorFile())
 	return
 
 if __name__ == '__main__':
